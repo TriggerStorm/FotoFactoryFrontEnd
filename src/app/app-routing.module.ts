52dc57e73b6b1207061cf8355bcd701fec25de1a
TriggerStorm <michael.h.p@hotmail.com>
2020-11-14 00:27:26 +0100

    + 1 path
    WE IN MOTHER FUCKER !

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './guards/auth.guards';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:4200'
+  apiUrl: 'https://localhost:44387'
 };
 
 /*
